6765ebca4ad2a57883363b887411a99f5eb50b92
Xue-LS <2845630436@qq.com>
2020-04-17 10:28:33 +0800

    xinxuetexiao

---
+++ js/data.js
@@ -0,0 +1,25 @@
+
+/*
+*   type :  0 -- DIV  1--JS  2--H5  3--Css3
+*
+*
+* */
+var flyData = [
+    {
+        type : "Css3",
+        author : "百度",
+        title : "效果",
+        dec : "效果展示………",
+        time : "2020-03-02",
+        src : "3D Drag"
+    },
+    {
+        type : "Js",
+        author : "打住",
+        title : "测试一下",
+        dec : "测试下…………",
+        time : "2020-03-02",
+        src : "3D Drag"
+    }
+];
+
